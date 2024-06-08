df4f7cc9b2151d74ec03f964208c9b642c3c34e9
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 19:45:29 +0200

    Add filterTodos to filter todos by search query, filter status

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,11 +3,23 @@ import { useAppSelector } from "../app/hooks";
 import Todo from "./Todo";
 const TodoList = () => {
   const todos = useAppSelector((state) => state.todoReducer.todos);
+  const filter = useAppSelector((state) => state.todoReducer.filter);
+  const searchQuery = useAppSelector((state) => state.todoReducer.searchQuery);
+  const filterTodos = todos.filter((todo) => {
+    const matchFilter =
+      filter === "All" ||
+      (filter === "completed" && todo.isCompleted) ||
+      (filter === "uncompleted" && !todo.isCompleted);
+    const matchSearchQuery = todo.title
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+    return matchFilter && matchSearchQuery;
+  });
   return (
     <ul className="flex flex-col gap-4 max-w-2xl mx-auto">
       <AnimatePresence>
-        {todos &&
-          todos.map((todo) => (
+        {filterTodos.length > 0 ? (
+          filterTodos.map((todo) => (
             <motion.li
               key={todo.id}
               initial={{ opacity: 0, y: -20 }}
@@ -17,7 +29,12 @@ const TodoList = () => {
             >
               <Todo todo={todo} />
             </motion.li>
-          ))}
+          ))
+        ) : (
+          <div className="flex items-center justify-center text-[#D72C63] text-3xl my-8">
+            No Matches!! :(
+          </div>
+        )}
       </AnimatePresence>
     </ul>
   );
