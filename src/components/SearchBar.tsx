0cc7895dc0260bbe24b78ba090729fce19788cec
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 19:41:18 +0200

    Add handleSearchQueryChange function to handle each change in input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,12 @@
 import { BiSearch } from "react-icons/bi";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { setSearchQuery } from "../rtk/features/todos/todoSlice";
 const SearchBar = () => {
+  const searchQuery = useAppSelector(state => state.todoReducer.searchQuery)
+  const dispatch = useAppDispatch()
+  const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchQuery(e.target.value))
+  }
   return (
     <form>
       <div className="flex items-center gap-2 px-3 py-1 rounded border border-[#D72C63]">
@@ -7,7 +14,9 @@ const SearchBar = () => {
           type="text"
           placeholder="Search by Name"
           aria-label="Search"
-          className="bg-transparent text-sm placeholder:text-[#0D0508]"
+          value={searchQuery}
+          onChange={handleSearchQueryChange}
+          className="bg-transparent text-sm placeholder:text-[#0D0508] outline-none"
         />
         <BiSearch fontSize="20" color="#D72C63" />
       </div>
