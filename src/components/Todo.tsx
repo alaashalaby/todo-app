8dde36003f90eac3f1f5844b5a069901100559b5
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 18:14:52 +0200

    Add handleDeleteTodo , handleToggele functions & displayed UpdateTodo Component when i want update todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,21 +1,58 @@
 import { BiEdit, BiTrash } from "react-icons/bi";
-
-const Todo = () => {
+import { useAppDispatch } from "../app/hooks";
+import { deleteTodo, toggleTodo } from "../rtk/features/todos/todoSlice";
+import { useState } from "react";
+import UpdateTodo from "./UpdateTodo";
+const Todo = ({ todo }: { todo: Todo }) => {
+  const dispatch = useAppDispatch();
+  const [isUpdated, setIsUpdated] = useState(false);
+  const handleDeleteTodo = (id: string) => {
+    dispatch(deleteTodo(id));
+  };
+  const handleToggle = (id: string) => {
+    dispatch(toggleTodo(id));
+  };
   return (
-    <li className="flex items-center p-3 rounded bg-[#F5F5F5] shadow-md">
-      <label className="flex items-center flex-1 relative cursor-pointer before:content-['']  before:w-4 before:h-4 before:border before:border-[#D72C63] before:rounded-full before:items-center before:justify-center before:absolute before:left-0}">
-        <input type="checkbox" className="hidden" />
-        <span className="ms-6">Todo</span>
-      </label>
-      <div className="flex gap-3 items-center">
-        <button className="text-[#D72C63]">
-          <BiEdit className="text-center text-xl" />
-        </button>
-        <button className="text-red-600">
-          <BiTrash className="text-center text-xl" />
-        </button>
-      </div>
-    </li>
+    <div className="flex items-center justify-between p-3 rounded bg-[#F5F5F5] shadow-md ">
+      {isUpdated ? (
+        <UpdateTodo todo={todo} setIsUpdated={setIsUpdated} />
+      ) : (
+        <>
+          <label className="flex items-center flex-1 relative cursor-pointer">
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              className="hidden peer"
+              onChange={() => handleToggle(todo.id)}
+            />
+            <span className="w-5 h-5 rounded-full border border-[#D72C63] absolute left-0 peer-checked:bg-[#D72C63] peer-checked:text-white peer-checked:text-sm peer-checked:font-bold flex items-center justify-center ">
+              {todo.isCompleted && "✓"}
+            </span>
+            <span
+              className={`ml-7 ${
+                todo.isCompleted ? "line-through text-[#7c7c7c] italic" : ""
+              }`}
+            >
+              {todo.title}
+            </span>
+          </label>
+          <div className="flex gap-3 items-center">
+            <button
+              className="text-[#D72C63]"
+              onClick={() => setIsUpdated(true)}
+            >
+              <BiEdit className="text-center text-xl" />
+            </button>
+            <button
+              className="text-red-600"
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              <BiTrash className="text-center text-xl" />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
