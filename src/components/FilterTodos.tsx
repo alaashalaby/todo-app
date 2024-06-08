afbdd4a44d486fe9969c1f86810e6a4003484211
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 19:49:27 +0200

    Add FilterTodos to display select of filter status

---
+++ src/components/FilterTodos.tsx
@@ -0,0 +1,25 @@
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { setFilter } from "../rtk/features/todos/todoSlice";
+
+const FilterTodos = () => {
+  const filter = useAppSelector((state) => state.todoReducer.filter);
+  const dispatch = useAppDispatch();
+  const handleChangeFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setFilter(e.target.value));
+  };
+  return (
+    <div className="flex items-center justify-center">
+      <select
+        className="border border-[#0D0508] outline-none rounded cursor-pointer py-1 text-[#0D0508]"
+        value={filter}
+        onChange={handleChangeFilter}
+      >
+        <option value="all">All</option>
+        <option value="uncompleted">UnCompleted</option>
+        <option value="completed">Completed</option>
+      </select>
+    </div>
+  );
+};
+
+export default FilterTodos;
