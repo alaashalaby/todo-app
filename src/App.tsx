ba2fc1ea983570feb3de166c06fe644e5844b0ac
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 19:50:58 +0200

    Add MainContent component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import MainContent from "./components/MainContent";
 import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <>
       <Navbar />
+      <main className="pt-28 container mx-auto">
+        <MainContent />
+      </main>
     </>
   );
 };
