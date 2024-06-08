28b941a6252f839d60f5b6472a2183e1afa6024d
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 18:19:08 +0200

    Add animation to Todo Element & display todo content

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,26 @@
-import Todo from "./Todo"
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useAppSelector } from "../app/hooks";
+import Todo from "./Todo";
 const TodoList = () => {
+  const todos = useAppSelector((state) => state.todoReducer.todos);
   return (
     <ul className="flex flex-col gap-4 max-w-2xl mx-auto">
-      <Todo />
-      <Todo />
+      <AnimatePresence>
+        {todos &&
+          todos.map((todo) => (
+            <motion.li
+              key={todo.id}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Todo todo={todo} />
+            </motion.li>
+          ))}
+      </AnimatePresence>
     </ul>
   );
-}
+};
 
-export default TodoList
+export default TodoList;
