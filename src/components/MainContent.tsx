5526f1ddee24cfd5c29b4deeebd038e74926e323
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-08 18:22:14 +0200

    Add MainContent display AddTodo & Todos component

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,12 @@
+import AddTodo from "./AddTodo";
+import Todos from "./Todos";
+const MainContent = () => {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+};
+
+export default MainContent;
