3f6bb8cd40223643632dec1fdcce908a61a653d2
Alaa Shalaby <alaashalabi011@gmail.com>
2024-06-09 21:45:32 +0200

    Add animation to SearchBar using framer-motion & change display of SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,26 +1,36 @@
 import { BiSearch } from "react-icons/bi";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { setSearchQuery } from "../rtk/features/todos/todoSlice";
+import { motion } from "framer-motion";
 const SearchBar = () => {
-  const searchQuery = useAppSelector(state => state.todoReducer.searchQuery)
-  const dispatch = useAppDispatch()
+  const searchQuery = useAppSelector((state) => state.todoReducer.searchQuery);
+  const dispatch = useAppDispatch();
   const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchQuery(e.target.value))
-  }
+    dispatch(setSearchQuery(e.target.value));
+  };
   return (
-    <form>
-      <div className="flex items-center gap-2 px-3 py-1 rounded border border-[#D72C63]">
-        <input
-          type="text"
-          placeholder="Search by Name"
-          aria-label="Search"
-          value={searchQuery}
-          onChange={handleSearchQueryChange}
-          className="bg-transparent text-sm placeholder:text-[#0D0508] outline-none"
-        />
-        <BiSearch fontSize="20" color="#D72C63" />
-      </div>
-    </form>
+    <motion.div
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
+      transition={{ duration: 0.5 }}
+      className="absolute top-20 right-0 w-full h-full 
+   mt-3 mb-10 px-4"
+    >
+      <form>
+        <div className="flex items-center justify-between gap-2 px-3 py-1 rounded border border-[#D72C63]">
+          <input
+            type="text"
+            placeholder="Search by Name"
+            aria-label="Search"
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+            className="bg-transparent text-sm placeholder:text-[#0D0508] outline-none"
+          />
+          <BiSearch fontSize="20" color="#D72C63" />
+        </div>
+      </form>
+    </motion.div>
   );
 };
 
