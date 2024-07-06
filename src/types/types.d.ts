6f7a695959b81863a4bbc9f51614e1ab39d7594a
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-07 01:56:27 +0200

    Add types of category

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -2,8 +2,14 @@ interface Todo {
   title: string;
   id: string;
   isCompleted: boolean;
+  category:Category
 }
 interface TodosProps {
   todos: Todo[];
   filter: string;
+}
+interface Category {
+  name: string;
+  color: string;
+  id: string;
 }
