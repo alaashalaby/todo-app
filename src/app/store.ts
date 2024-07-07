4b941bf1c457e44d8c5005d298cf76b5ba3074be
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-07 02:13:00 +0200

    Add CategoriesReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../rtk/features/todos/todoSlice";
+import categoriesReducer from "../rtk/features/categories-slice/categoriesSlice";
 const store = configureStore({
   reducer: {
     todoReducer,
+    categoriesReducer
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
