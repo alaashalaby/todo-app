e2c68a8a3bb6bbe5e669a808ea861dd5246bcb51
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-07 23:46:57 +0200

    Delete FilterTodos from MainContent and Add to AddTodo Component

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,9 @@
 import AddTodo from "./AddTodo";
-import FilterTodos from "./FilterTodos";
 import Todos from "./Todos";
 const MainContent = () => {
   return (
     <>
       <AddTodo />
-      <FilterTodos/>
       <Todos />
     </>
   );
