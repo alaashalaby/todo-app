6a3f778283aa02c2fe644dd50d2b440d78dca156
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-07 21:46:11 +0200

    Edit ui of Todo & Add Time & CategoryName

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from "../app/hooks";
 import { deleteTodo, toggleTodo } from "../rtk/features/todos/todoSlice";
 import { useState } from "react";
 import UpdateTodo from "./UpdateTodo";
+import { formatTime } from "../utils/formatTime";
 const Todo = ({ todo }: { todo: Todo }) => {
   const dispatch = useAppDispatch();
   const [isUpdated, setIsUpdated] = useState(false);
@@ -13,42 +14,53 @@ const Todo = ({ todo }: { todo: Todo }) => {
     dispatch(toggleTodo(id));
   };
   return (
-    <div className="flex items-center justify-between p-3 rounded bg-[#F5F5F5] shadow-md ">
+    <div className="flex flex-col gap-2 p-6 rounded bg-[#F5F5F5] shadow-md ">
       {isUpdated ? (
         <UpdateTodo todo={todo} setIsUpdated={setIsUpdated} />
       ) : (
         <>
-          <label className="flex items-center flex-1 relative cursor-pointer">
-            <input
-              type="checkbox"
-              checked={todo.isCompleted}
-              className="hidden peer"
-              onChange={() => handleToggle(todo.id)}
-            />
-            <span className="w-5 h-5 rounded-full border border-[#D72C63] absolute left-0 peer-checked:bg-[#D72C63] peer-checked:text-white peer-checked:text-sm peer-checked:font-bold flex items-center justify-center ">
-              {todo.isCompleted && "✓"}
-            </span>
+          <div className="flex items-center justify-between">
+            <label className="flex items-center flex-1 relative cursor-pointer">
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                className="hidden peer"
+                onChange={() => handleToggle(todo.id)}
+              />
+              <span className="w-5 h-5 rounded-full border border-[#D72C63] absolute left-0 peer-checked:bg-[#D72C63] peer-checked:text-white peer-checked:text-sm peer-checked:font-bold flex items-center justify-center ">
+                {todo.isCompleted && "✓"}
+              </span>
+              <span
+                className={`ml-7 ${
+                  todo.isCompleted ? "line-through text-[#7c7c7c] italic" : ""
+                }`}
+              >
+                {todo.title}
+              </span>
+            </label>
+            <div className="flex gap-3 items-center">
+              <button
+                className="text-[#D72C63]"
+                onClick={() => setIsUpdated(true)}
+              >
+                <BiEdit className="text-center text-xl" />
+              </button>
+              <button
+                className="text-red-600"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                <BiTrash className="text-center text-xl" />
+              </button>
+            </div>
+          </div>
+          <div className="pt-5 pb-3 flex items-center justify-between">
             <span
-              className={`ml-7 ${
-                todo.isCompleted ? "line-through text-[#7c7c7c] italic" : ""
-              }`}
+              style={{ backgroundColor: todo.category.color }}
+              className="px-4 py-2 text-sm rounded-full text-white"
             >
-              {todo.title}
+              {todo.category.name.toUpperCase()}
             </span>
-          </label>
-          <div className="flex gap-3 items-center">
-            <button
-              className="text-[#D72C63]"
-              onClick={() => setIsUpdated(true)}
-            >
-              <BiEdit className="text-center text-xl" />
-            </button>
-            <button
-              className="text-red-600"
-              onClick={() => handleDeleteTodo(todo.id)}
-            >
-              <BiTrash className="text-center text-xl" />
-            </button>
+            <span>{todo && formatTime(new Date(todo?.time))}</span>
           </div>
         </>
       )}
