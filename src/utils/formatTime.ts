5584c5f4d893ed96b898b272d4e58c1e75c42bce
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-07 21:38:27 +0200

    Update function to display date instead of time

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,8 +1,13 @@
 export const formatTime = (date: Date): string => {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const format = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12;
-  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
-  return `${formattedHours}:${formattedMinutes} ${format}`;
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  // const hours = date.getHours();
+  // const minutes = date.getMinutes();
+  // const format = hours >= 12 ? "PM" : "AM";
+  // const formattedHours = hours % 12;
+  // const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+  // const formattedTime = `${formattedHours}:${formattedMinutes} ${format}`;
+  const formattedDate = `${day}.${month}.${year}`;
+  return `${formattedDate} `;
 };
