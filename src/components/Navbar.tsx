f333871349f6d09aef02607266b48a96757fbebb
Alaa Shalaby <alaashalabi011@gmail.com>
2024-07-08 15:20:19 +0200

    Add ChangeLangButtons component to Navbar & Add className flex to nav item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { BiSearch, BiX } from "react-icons/bi";
 import logo from "../assets/logo.png";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
+import ChangeLangButtons from "./ChangeLangButtons";
 const Navbar = () => {
   const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
   const handleToggleSearchBar = () => {
@@ -11,13 +12,14 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 z-50 shadow-md md:shadow-sm w-full">
       <div className="container mx-auto flex justify-between items-center p-2 relative">
         <img src={logo} alt="todo logo" className="w-16" />
-        <nav>
+        <nav className="flex gap-3 items-center">
           <button
             onClick={handleToggleSearchBar}
             className="text-xl text-[#D72C63] border border-[#D72C63] p-2 rounded-full transition-all duration-[0.3s] ease-in-out hover:bg-[#D72C63] hover:text-white"
           >
             {isSearchBarVisible ? <BiX /> : <BiSearch />}
           </button>
+          <ChangeLangButtons/>
           {isSearchBarVisible && <SearchBar />}
         </nav>
       </div>
